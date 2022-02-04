7492c76998cf67769088210af31660e7e1745915
Sebastian Belmonte <sebastianbelmonte95@gmail.com>
2022-02-03 20:15:09 -0600

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+if __name__ = "__main__":
+    setup(name = "mypackage",
+          description = "this is mypackage description",
+          license="MIT",
+          url="https://github.com/SebastianBelmonte95/mypackage",
+          version = "0.0.1",
+          author = "Sebastian Belmonte", 
+          author_email = "sebastian@email.com",
+          long_description=open("README.md").read(),
+          packages = find_packages(),
+          zip_safe = False,
+          install_requires = [],
+          classifiers=[
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "Natural Language :: English",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+],
+          )
